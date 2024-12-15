5f238dd7ae030f5e27785033fddbaa05577be979
just-doks <graviman13@gmail.com>
2024-12-15 12:24:24 +0300

    Aston_js-2 [feat]: Made experiments with compare func

--- lesson_2/explore/exploreCompare.js
+++ lesson_2/explore/exploreCompare.js
@@ -30,6 +30,7 @@ class Queue {
 
 function BFS(initNode) {
     let visited = new Map();
+    visited.set(Symbol(initNode), true);
     let q = new Queue();
     q.enqueue(initNode);
     while (!q.isEmpty()) {
@@ -38,7 +39,6 @@ function BFS(initNode) {
 
         console.log(node.value);
 
-        visited.set(Symbol(node), true);
         if (node.left && !visited.get(Symbol(node.left))) {
             q.enqueue(node.left);
             visited[Symbol(node.left)] = true;
@@ -62,39 +62,67 @@ const node2_1l = new Node(2, node4_2l, node5_2r)
 const node1 = new Node(1, node2_1l, node3_1r)
 
 
-BFS(node1)
+function deepCopy(initNode) {
+    let newNode = new Node(null);
+    if (!(initNode instanceof Node))
+        return newNode;
+    let q = new Queue();
+    q.enqueue([initNode, newNode]);
+    while (!q.isEmpty()) {
+        const [nodeCopy, nodePaste] = q.dequeue();
 
-function deepCopy(obj) {
-    if (typeof obj !== 'object' || obj === null) {
-        return obj;
+        nodePaste.value = nodeCopy.value;
+
+        if (nodeCopy.left) {
+            nodePaste.left = new Node(null);
+            q.enqueue([nodeCopy.left, nodePaste.left]);
+        }
+        if (nodeCopy.right) {
+            nodePaste.right = new Node(null);
+            q.enqueue([nodeCopy.right, nodePaste.right]);
+        }
     }
-    if (Array.isArray(obj)) {
-        return obj.map((el) => deepCopy(el));
-    }
-    return Object.fromEntries(
-        Object.entries(obj).map(([key, value]) => 
-            ([key, deepCopy(value)])));
+    return newNode;
 }
 
 function compare(n1, n2) {
-    let visited = new Map();
     let q = new Queue();
     q.enqueue([n1,n2]);
     while (!q.isEmpty()) {
         const [node1, node2] = q.dequeue();
-        if (!(node1 instanceof Node) || !(node2 instanceof Node))
-            return false;
         if (node1.value !== node2.value)
             return false;
 
-        visited.set(Symbol(node1), true);
+        if (node1.left && node2.left) {
+            q.enqueue([node1.left, node2.left]);
+        } else
+        if (node1.left !== node2.left)
+            return false;
+        if (node1.right && node2.right) {
+            q.enqueue([node1.right, node2.right]);
+        } else 
+        if (node1.right !== node2.right)
+            return false;
+    }
+    return true;
+}
+
+function compareVisit(n1, n2) {
+    let visited = new Map();
+    visited.set(Symbol(n1), true);
+    let q = new Queue();
+    q.enqueue([n1,n2]);
+    while (!q.isEmpty()) {
+        const [node1, node2] = q.dequeue();
+        if (node1.value !== node2.value)
+            return false;
+
         if (node1.left && node2.left && !visited.get(Symbol(node1.left))) {
             q.enqueue([node1.left, node2.left]);
             visited.set(Symbol(node1.left), true);
         } else
         if (node1.left !== node2.left)
             return false;
-
         if (node1.right && node2.right && !visited.get(Symbol(node1.right))) {
             q.enqueue([node1.right, node2.right]);
             visited.set(Symbol(node1.right), true);
@@ -105,11 +133,42 @@ function compare(n1, n2) {
     return true;
 }
 
+function compareNodes(n1, n2) {
+    if (!(n1 instanceof Node) || !(n2 instanceof Node))
+        return false;
+    let q = new Queue();
+    q.enqueue([n1,n2]);
+    while (!q.isEmpty()) {
+        const [node1, node2] = q.dequeue();
+        if (node1.value !== node2.value)
+            return false;
+        if (node1.left && node2.left) {
+            if (!(node1 instanceof Node) || !(node2 instanceof Node))
+                return false;
+            q.enqueue([node1.left, node2.left]);
+        } else
+        if (node1.left !== node2.left)
+            return false;
+        if (node1.right && node2.right) {
+            if (!(node1 instanceof Node) || !(node2 instanceof Node))
+                return false;
+            q.enqueue([node1.right, node2.right]);
+        } else 
+        if (node1.right !== node2.right)
+            return false;
+    }
+    return true;
+}
+
+
 const nodeA = node1;
 const nodeB = deepCopy(nodeA);
-console.log(compare(nodeA, nodeB));
+//BFS(nodeB)
+//console.log()
+console.log(compareNodes(nodeA, nodeB));
+nodeB.left.right.value = 99;
+console.log(compareNodes(nodeA, nodeB));
 nodeA.left.right = undefined;
+console.log(compareNodes(nodeA, nodeB));
 nodeB.left.right = undefined;
-console.log(compare(nodeA, nodeB));
-nodeB.right.right.value = 99;
-console.log(compare(nodeA, nodeB));
+console.log(compareNodes(nodeA, nodeB));
